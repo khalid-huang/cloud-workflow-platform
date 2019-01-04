4c3649ea659063f1f24478d5a821d7c173467cc3
sysuKinthon <qinkai1994@qq.com>
2019-01-04 22:26:53 +0800

    update

--- code/back-end/management-service/multi-tenant-management-service/src/main/java/org/sysu/multitenantmanagementservice/service/TenantManageService.java
+++ code/back-end/management-service/multi-tenant-management-service/src/main/java/org/sysu/multitenantmanagementservice/service/TenantManageService.java
@@ -1,4 +1,17 @@
 package org.sysu.multitenantmanagementservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.sysu.multitenantmanagementservice.Entity.TenantEntityRepository;
+
+
+@Service
 public class TenantManageService {
+    private static Logger logger = LoggerFactory.getLogger(TenantManageService.class);
+
+    @Autowired
+    private TenantEntityRepository tenantEntityRepository;
+
 }
