a75d618a29b5b0673cfff9d4eb4e0f0ca604a5af
sysuKinthon <qinkai1994@qq.com>
2019-01-04 21:56:41 +0800

    update sql

--- code/back-end/management-service/multi-tenant-management-service/src/main/java/org/sysu/multitenantmanagementservice/Entity/ProcessEngineEntityRepository.java
+++ code/back-end/management-service/multi-tenant-management-service/src/main/java/org/sysu/multitenantmanagementservice/Entity/ProcessEngineEntityRepository.java
@@ -4,5 +4,5 @@ package org.sysu.multitenantmanagementservice.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProcessEngineEntityRepository extends JpaRepository<ProcessEngineEntity, Long> {
-    ProcessEngineEntity findByReAndResourceId(Long resourceId);
+    ProcessEngineEntity findByResourceId(Long resourceId);
 }
