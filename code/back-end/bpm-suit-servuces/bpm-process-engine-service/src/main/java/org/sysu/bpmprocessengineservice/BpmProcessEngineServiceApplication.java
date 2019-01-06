4110bc54c5afbae635379c1f8693a8dbce1e868e
sysuKinthon <qinkai1994@qq.com>
2019-01-06 11:22:36 +0800

    初始化BPM套件项目

---
+++ code/back-end/bpm-suit-servuces/bpm-process-engine-service/src/main/java/org/sysu/bpmprocessengineservice/BpmProcessEngineServiceApplication.java
@@ -0,0 +1,31 @@
+package org.sysu.bpmprocessengineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class BpmProcessEngineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmProcessEngineServiceApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
+
