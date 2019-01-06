0cb9e9bc24001e7583449cebf35053c0402db8eb
sysuKinthon <qinkai1994@qq.com>
2019-01-06 17:29:01 +0800

    修改engine-service

---
+++ code/back-end/bpm-suit-servuces/bpm-process-engine-service/src/main/java/org/sysu/bpmprocessengineservice/controller/EngineController.java
@@ -0,0 +1,28 @@
+package org.sysu.bpmprocessengineservice.controller;
+
+import org.activiti.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.sysu.bpmprocessengineservice.service.EngineService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EngineController {
+    private final static Logger logger = LoggerFactory.getLogger(EngineController.class);
+
+    @Autowired
+    EngineService engineService;
+
+    @RequestMapping(value = "/startProcessInstanceByKey/{processModelKey}", method = RequestMethod.POST)
+    public ResponseEntity<?> startProcessInstanceByKey(@PathVariable(value = "processModelKey") String processModelKey,
+                                                       @RequestParam Map<String, Object> variables) {
+        HashMap<String,String> response = new HashMap<>();
+//        ProcessInstance pi = engineService.
+        return null;
+    }
+}
--- code/back-end/bpm-suit-servuces/bpm-process-engine-service/src/main/java/org/sysu/bpmprocessengineservice/service/impl/EngineServiceImpl.java
+++ code/back-end/bpm-suit-servuces/bpm-process-engine-service/src/main/java/org/sysu/bpmprocessengineservice/service/impl/EngineServiceImpl.java
@@ -1,6 +1,7 @@
 package org.sysu.bpmprocessengineservice.service.impl;
 
 
+import com.alibaba.fastjson.JSON;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -9,8 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.sysu.bpmprocessengineservice.constant.ResponseConstantManager;
 import org.sysu.bpmprocessengineservice.service.EngineService;
 
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,48 +34,92 @@ public class EngineServiceImpl implements EngineService {
 
 
     @Override
-    public ProcessInstance startProcessInstanceByKey(String processModelKey, Map<String, Object> variables) {
-        return runtimeService.startProcessInstanceByKey(processModelKey, variables);
+    public Map<String, String> startProcessInstanceByKey(String processModelKey, Map<String, Object> variables) {
+        HashMap<String, String> response = new HashMap<>();
+        ProcessInstance pi =  runtimeService.startProcessInstanceByKey(processModelKey, variables);
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        response.put("processInstanceId", pi.getId());
+        response.put("processDefinitionId", pi.getProcessDefinitionId());
+        return response;
     }
 
     @Override
-    public ProcessInstance startProcessInstanceById(String processDefinitionId, Map<String, Object> variables) {
-        return runtimeService.startProcessInstanceById(processDefinitionId, variables);
+    public Map<String, String> startProcessInstanceById(String processDefinitionId, Map<String, Object> variables) {
+        HashMap<String, String> response = new HashMap<>();
+        ProcessInstance pi =  runtimeService.startProcessInstanceById(processDefinitionId, variables);
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        response.put("processInstanceId", pi.getId());
+        response.put("processDefinitionId", pi.getProcessDefinitionId());
+        return response;
     }
 
     @Override
-    public List<Task> getCurrentTasksForAssign(String processInstanceId, String assignee) {
-        return taskService.createTaskQuery().processInstanceId(processInstanceId).taskAssignee(assignee).list();
+    public Map<String, String> getCurrentTasksForAssign(String processInstanceId, String assignee) {
+        HashMap<String, String> response = new HashMap<>();
+        List<Task> tasks=  taskService.createTaskQuery().processInstanceId(processInstanceId).taskAssignee(assignee).list();
+        List<String> taskIds = new ArrayList<>();
+        for(Task task : tasks) {
+            taskIds.add(task.getId());
+        }
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        response.put("taskIds", JSON.toJSONString(taskIds));
+        return response;
     }
 
     @Override
-    public List<Task> getCureentTask(String processInstanceId) {
-        return taskService.createTaskQuery().processInstanceId(processInstanceId).list();
+    public Map<String, String> getCurrentTasks(String processInstanceId) {
+        HashMap<String, String> response = new HashMap<>();
+        List<Task> tasks =taskService.createTaskQuery().processInstanceId(processInstanceId).list();
+        List<String> taskIds = new ArrayList<>();
+        for(Task task : tasks) {
+            taskIds.add(task.getId());
+        }
+        response.put("taskIds", JSON.toJSONString(taskIds));
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        return response;
     }
 
     @Override
-    public Task getCurrentSingleTask(String processInstanceId) {
-        return taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+    public Map<String, String> getCurrentSingleTask(String processInstanceId) {
+        HashMap<String, String> response = new HashMap<>();
+        Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        response.put("taskId", task.getId());
+        return response;
     }
 
     @Override
-    public void claimTask(String taskId, String assignee) {
+    public Map<String, String> claimTask(String taskId, String assignee) {
+        HashMap<String, String> response = new HashMap<>();
         taskService.claim(taskId, assignee);
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        return response;
     }
 
     @Override
-    public void completeTask(String taskId, Map<String, Object> variables) {
+    public Map<String, String> completeTask(String taskId, Map<String, Object> variables) {
+        HashMap<String, String> response = new HashMap<>();
         taskService.complete(taskId, variables);
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        return response;
     }
 
     @Override
-    public boolean isEnded(String processInstanceId) {
+    public Map<String, String> isEnded(String processInstanceId) {
+        HashMap<String, String> response = new HashMap<>();
         ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        int flag = 0; //0表示未结束，1表示已经结束
         //如果事务提交之后，流程实例会被删除的，所以要判断
         if(processInstance != null) {
-            return processInstance.isEnded();
+            flag = processInstance.isEnded() ? 1 : 0;
+        } else {
+            //如果已经没有这个流程实例了，表示已经执行完成了
+            flag = 1;
         }
-        //如果已经没有这个流程实例了，表示已经执行完成了
-        return true;
+        response.put("status", ResponseConstantManager.STATUS_SUCCESS);
+        response.put("isEnd", "" + flag);
+        return response;
     }
+
+    private
 }
