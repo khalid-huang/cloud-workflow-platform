75844ce9ebd42783d40fc42ac7b299547da6b3b4
sysuKinthon <qinkai1994@qq.com>
2019-01-11 22:06:16 +0800

    bpm management service

---
+++ code/back-end/bpm-suit-services/bpm-management-service/src/main/java/org/sysu/bpmmanagementservice/config/ActivitiConfig.java
@@ -0,0 +1,102 @@
+package org.sysu.bpmmanagementservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.engine.*;
+import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.activiti.image.impl.DefaultProcessDiagramGenerator;
+import org.activiti.spring.ProcessEngineFactoryBean;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by liuruijie on 2017/2/20.
+ * activiti工作流配置
+ */
+@Configuration
+public class ActivitiConfig {
+
+//    @Value("${dataType}")
+    private String dataType = "mysql";
+
+    /**
+     * spring 集成 activiti
+     */
+    @Bean
+    public ProcessEngineConfiguration processEngineConfiguration(DataSource dataSource, PlatformTransactionManager transactionManager) {
+        SpringProcessEngineConfiguration processEngineConfiguration = new SpringProcessEngineConfiguration();
+        processEngineConfiguration.setDataSource(dataSource);
+        //表不存在创建表
+        processEngineConfiguration.setDatabaseSchemaUpdate("true");
+        //指定数据库
+        processEngineConfiguration.setDatabaseType(dataType);
+        processEngineConfiguration.setTransactionManager(transactionManager);
+        //历史变量
+        processEngineConfiguration.setHistory("full");
+        //指定字体
+        processEngineConfiguration.setActivityFontName("宋体");
+        processEngineConfiguration.setAnnotationFontName("宋体");
+        processEngineConfiguration.setLabelFontName("宋体");
+
+        processEngineConfiguration.setProcessDiagramGenerator(new DefaultProcessDiagramGenerator());
+
+        return processEngineConfiguration;
+    }
+
+    //流程引擎，与spring整合使用factoryBean
+    @Bean
+    public ProcessEngineFactoryBean processEngine(ProcessEngineConfiguration processEngineConfiguration) {
+        ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
+        processEngineFactoryBean.setProcessEngineConfiguration((ProcessEngineConfigurationImpl) processEngineConfiguration);
+        return processEngineFactoryBean;
+    }
+
+    @Bean
+    public RepositoryService repositoryService(ProcessEngine processEngine) {
+        return processEngine.getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService(ProcessEngine processEngine) {
+        return processEngine.getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService(ProcessEngine processEngine) {
+        return processEngine.getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService(ProcessEngine processEngine) {
+        return processEngine.getHistoryService();
+    }
+
+    @Bean
+    public FormService formService(ProcessEngine processEngine) {
+        return processEngine.getFormService();
+    }
+
+    @Bean
+    public IdentityService identityService(ProcessEngine processEngine) {
+        return processEngine.getIdentityService();
+    }
+
+    @Bean
+    public ManagementService managementService(ProcessEngine processEngine) {
+        return processEngine.getManagementService();
+    }
+
+    @Bean
+    public DynamicBpmnService dynamicBpmnService(ProcessEngine processEngine) {
+        return processEngine.getDynamicBpmnService();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
