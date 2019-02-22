d9175c7fdd0e4bccf26f2028964e3892a8b869f3
sysuKinthon <qinkai1994@qq.com>
2019-02-22 16:28:03 +0800

    bmp process-engines-portal项目建立

---
+++ code/back-end/bpm-suit-services/bpm-process-engines-portal/src/main/java/org/sysu/bpmprocessenginesportal/BpmProcessEnginesPortalApplication.java
@@ -0,0 +1,13 @@
+package org.sysu.bpmprocessenginesportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmProcessEnginesPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmProcessEnginesPortalApplication.class, args);
+    }
+
+}
---
+++ code/back-end/bpm-suit-services/bpm-process-engines-portal/src/test/java/org/sysu/bpmprocessenginesportal/BpmProcessEnginesPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sysu.bpmprocessenginesportal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BpmProcessEnginesPortalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
